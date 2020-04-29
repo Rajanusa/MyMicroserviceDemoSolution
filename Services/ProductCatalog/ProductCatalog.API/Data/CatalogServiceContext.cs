07644b5b3c808bd5708abc3493f90e6693217b49
Palanivelrajan <pklpvr@outlook.com>
2020-04-28 22:12:46 -0500

    Initial Commit

---
+++ Services/ProductCatalog/ProductCatalog.API/Data/CatalogServiceContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductCatalog.API.Domain;
+
+    public class CatalogServiceContext : DbContext
+    {
+        public CatalogServiceContext (DbContextOptions<CatalogServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductCatalog.API.Domain.CatalogItem> CatalogItems { get; set; }
+
+        public DbSet<ProductCatalog.API.Domain.CatalogBrand> CatalogBrands { get; set; }
+
+        public DbSet<ProductCatalog.API.Domain.CatalogType> CatalogTypes { get; set; }
+    }
---
+++ Services/ProductCatalog/ProductCatalog.API/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ProductCatalog.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<CatalogServiceContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CatalogServiceContext")));
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "Http Web API Demo",
+                    Version = "v1",
+                    Description = "This is a demo of how to use Swager in Web API"
+
+                });
+            });
+
+
+
+            services.AddDbContext<CatalogServiceContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CatalogBrandServiceContext")));
+
+            services.AddDbContext<CatalogServiceContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CatalogTypeServiceContext")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger().UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+        }
+    }
+}
